a9a94fcafa773a4197c46094f9d16aae1f389f16
LanceeAce <lanceace019@gmail.com>
2025-09-09 11:26:51 +0900

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="shadow-md bg-[#FFF5D0]   md:px-20 px-10 py-4 flex  md:items-center justify-between md:relative">
+      {/* Logo */}
+      <div className="flex items-center gap-2">
+        <img src={logo} alt="Logo" className="h-15"/>
+        <h1 className="text-[2rem] font-bold text-gray-800">Pet Connect</h1>
+      </div>
+
+      {/* Burger Menu Button (Mobile) */}
+      <div className="md:hidden flex items-center">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-gray-700 focus:outline-none"
+        >
+          {/* Hamburger icon */}
+          {isOpen ? (
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
+
+      {/* Navigation */}
+      <nav
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } flex-col gap-4 mt-12 md:mt-0 md:flex md:flex-row items-center bg-amber-50 md:gap-6 text-gray-700 font-medium md:relative absolute right-5`}
+      >
+        <a href="#" className="hover:text-orange-500 transition">
+          Home
+        </a>
+        <a href="#" className="hover:text-orange-500 transition">
+          Adopt
+        </a>
+        <a href="#" className="hover:text-orange-500 transition">
+          Form
+        </a>
+        <a href="#" className="hover:text-orange-500 transition">
+          About
+        </a>
+
+        {/* Login Button */}
+        <button className="bg-orange-400 hover:bg-orange-500 text-white font-medium px-4 py-2 rounded-lg md:ml-4">
+          Login
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Home.tsx
@@ -0,0 +1,25 @@
+import owner from '../assets/owner.png';
+
+const Home = () => {
+  return (
+    <div className='md:flex justify-between md:px-20 px-10 bg-[#FFF5D0]'>
+        <div className="text-center mt-1 md:mt-35 pt-10">
+            <h1 className="md:text-[3rem] text-[2rem]  font-bold mb-4 md:w-[26rem] text-left">Find Your New
+Best Friend Today</h1>
+            <p className="md:text-[1.8rem] text-gray-600 md:w-[30rem] text-left">Browse hundreds of loving pets waiting for their forever homes.</p>
+            <div className='md:w-[10rem] pt-2 md:pt-5 md:ml-[5rem]'>
+             <p
+      className="bg-[#EA824E] text-white px-6 py-3 rounded-lg font-medium shadow-md transition-all duration-300 transform hover:bg-[#d76f3e] hover:scale-105"
+    >
+      Browse Pets
+    </p>
+    </div>
+        </div>
+        <div className="">
+            <img src={owner} alt="Owner with pet" className="md:h-[43rem]"/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
